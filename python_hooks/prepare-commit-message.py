44943faea090db4aca5c69372f932ed7975da1f6
msmirnova <marina.smirnova@tabby.ai>
2023-04-05 14:15:49 +0300

    try to fix

--- python_hooks/prepare-commit-message.py
+++ python_hooks/prepare-commit-message.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import re
 import sys
 from subprocess import check_output
@@ -9,12 +7,14 @@ branch = (check_output(["git", "symbolic-ref", "--short", "HEAD"]).decode("utf-8
 
 regex = r"^[A-Z]{1,3}-[0-9]{1,9}"
 
-found_obj = re.match(regex, branch)
 
-if found_obj:
-    prefix = found_obj.group(0)
-    with open(commit_msg_filepath, "r+") as f:
-       commit_msg = f.read()
-       if commit_msg.find(prefix) == -1:
-             f.seek(0, 0)
-             f.write(f"[{prefix}] {commit_msg}")
+def perepare():
+    found_obj = re.match(regex, branch)
+
+    if found_obj:
+        prefix = found_obj.group(0)
+        with open(commit_msg_filepath, "r+") as f:
+            commit_msg = f.read()
+            if commit_msg.find(prefix) == -1:
+                f.seek(0, 0)
+                f.write(f"[{prefix}] {commit_msg}")
