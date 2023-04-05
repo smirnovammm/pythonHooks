2c3ceecf89c01cbd79b9fcbbc249ec3f0a3e044f
msmirnova <marina.smirnova@tabby.ai>
2023-04-05 17:47:17 +0300

    Fix hook

--- python_hooks/__init__.py
+++ python_hooks/__init__.py
@@ -3,21 +3,18 @@ import sys
 from subprocess import check_output
 
 red_color = "\033[1;31m"
+commit_msg_filepath = sys.argv[1]
+branch = (check_output(["git", "symbolic-ref", "--short", "HEAD"]).decode("utf-8").strip())
+
+regex = r"^[a-zA-Z]{1,5}-[0-9]{1,9}"
 
 
 def perepare_commit_msg():
-    commit_msg_filepath = sys.argv[1]
-    branch = (check_output(["git", "symbolic-ref", "--short", "HEAD"]).decode("utf-8").strip())
-
-    regex = r"^[a-zA-Z]{1,5}-[0-9]{1,9}"
-
     if re.search(regex, branch):
+        found_obj = re.match(regex, branch)
+        prefix = found_obj.group(0)
         with open(commit_msg_filepath, "r+") as file:
             commit_msg = file.read()
-            found_obj = re.match(regex, branch)
-            prefix = found_obj.group(0)
-            print(red_color + f"{prefix}")
-            print(red_color + f"{branch}")
             if commit_msg.find(prefix) == -1:
                 file.seek(0, 0)
                 file.write(f"[{prefix}] {commit_msg}")
