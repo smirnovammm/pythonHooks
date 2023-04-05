a27a806a3f0d33dba9f918ccf5926af285213ead
msmirnova <marina.smirnova@tabby.ai>
2023-04-05 15:30:49 +0300

    Fix hook

--- python_hooks/__init__.py
+++ python_hooks/__init__.py
@@ -2,6 +2,8 @@ import re
 import sys
 from subprocess import check_output
 
+red_color = "\033[1;31m"
+
 
 def perepare_commit_msg():
     commit_msg_filepath = sys.argv[1]
@@ -9,13 +11,17 @@ def perepare_commit_msg():
 
     regex = r"^[A-Z]{1,3}-[0-9]{1,9}"
 
-    found_obj = re.match(regex, branch)
-
-    if found_obj:
-        prefix = found_obj.group(0)
-        with open(commit_msg_filepath, "r+") as f:
-            commit_msg = f.read()
+    with open(commit_msg_filepath, "r+") as file:
+        commit_msg = file.read()
+        if re.search(regex, branch):
+            found_obj = re.match(regex, branch)
+            prefix = found_obj.group(0)
             if commit_msg.find(prefix) == -1:
-                f.seek(0, 0)
-                f.write(f"[{prefix}] {commit_msg}")
+                file.seek(0, 0)
+                file.write(f"[{prefix}] {commit_msg}")
+        else:
+            print(red_color + "Branch name does not contain ticket number")
+            print("commit-msg hook failed")
+            sys.exit(1)
+
 
